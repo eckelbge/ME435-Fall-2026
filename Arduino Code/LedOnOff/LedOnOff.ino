e2bd96246b15395e70e4fa10e94b7d683928c76a
eckelbge <eckelbge@rose-hulman.edu>
2026-09-08 14:10:57 -0400

    Arduino On Off Program

---
+++ Arduino Code/LedOnOff/LedOnOff.ino
@@ -0,0 +1,37 @@
+String inputString = "";
+bool isStringComplete = false;
+
+void setup() {
+  Serial.begin(19200);
+  inputString.reserve(200);
+  pinMode(13,OUTPUT);
+}
+
+void loop() {
+  if (isStringComplete) {
+    if(inputString.equals("LED ON")){
+      digitalWrite(13,HIGH);
+      Serial.println("The LED is now on!");
+    } else if(inputString.equals("LED OFF")){
+      digitalWrite(13,LOW);
+      Serial.println("The LED is now off!");
+    } else{
+      Serial.print("Unknown command -->");
+      Serial.println(inputString);
+    }
+
+    inputString = "";
+    isStringComplete = false;
+  }
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    if (inChar == '\n') {
+      isStringComplete = true;
+    } else {
+      inputString += inChar;
+    }
+  }
+}
