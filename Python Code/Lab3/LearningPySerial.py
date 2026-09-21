60ef7db0b01481d4b1b90b5b9082138cd1fc21c6
eckelbge <eckelbge@rose-hulman.edu>
2026-09-21 14:31:49 -0400

    y

--- Python Code/Lab3/LearningPySerial.py
+++ Python Code/Lab3/LearningPySerial.py
@@ -1,13 +1,23 @@
 import serial
+import time
 
 print("Learning Pyserial")
 
 ser = serial.Serial("COM6",19200,timeout=10)
 
-while not ser.is_open:
-    print("Opening...")
+time.sleep(2.0) #Necessary sometimes
 
-#TODO: Use the ser object
+ser.reset_output_buffer()
+message="RESET"
+print(message)
+message_bytes=(message+"\n").encode()
+print(message_bytes)
 
+ser.write(message_bytes)
+
+response_bytes=ser.readline()
+print(response_bytes)
+response=response_bytes.decode().strip()
+print(response)
 
 ser.close()
